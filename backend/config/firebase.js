1db7e51e56625bc112c38a17aee736e50328376d
anubhav kumar <anubhavkumar@anubhavs-Mac-mini.local>
2025-10-08 12:04:28 +0530

    firebase problem

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-
+import dotenv from "dotenv";
+dotenv.config();
+console.log("Storage bucket:", process.env.FIREBASE_STORAGE_BUCKET);
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
